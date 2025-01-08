0b1847671729d899fce11c9e4c6030048e324440
mp022 <mpferreiradesouza@gmail.com>
2025-01-08 16:53:14 -0300

    update: atualiza a main

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
-def soma(a :int, b: int):
-    return a + b
+# Padrão: Anaconda 22.9 e/ou Python +3.9 com NumPy, SciPy e Pandas.
+# Bibliotecas: Networkx, iGraph.
 
-print("hello world!")
-print(soma(1,1))
+import numpy
+import scipy
+import pandas
+import networkx
+import igraph
+
+## Preicsa implementar: ##
+# algoritmo branch-and-bound, 
+# algoritmo twice-around-the-tree, 
+# algoritmo de Christofides 
+## para solucionar o problema do caixeiro viajante geométrico. ##
+
+# deverão avaliar o desempenho dos algoritmos segundo três aspectos: tempo, espaço, e qualidade da solução.
+# tempo de processamento deve ser limitado a 30min. dados referentes à execução colocados como NA (não-disponível).
+
+print("Hello Wolrd!")
+
